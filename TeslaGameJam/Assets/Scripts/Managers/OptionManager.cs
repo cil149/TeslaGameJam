418e60c42d085ccf81c19e0e2ea298d010fe52ca
KyTn <angel.kytn@gmail.com>
2017-01-22 02:17:35 +0100

    commit

---
+++ TeslaGameJam/Assets/Scripts/Managers/OptionManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionManager : MonoBehaviour {
+
+    public static OptionManager instance;
+
+    public bool _InverseXAxis, _InverseYAxis, _EnableMusic, _EnableSound; 
+
+	// Use this for initialization
+	void Awake () {
+        DontDestroyOnLoad(gameObject);
+        instance = this;
+	}
+
+    public void defaultValues()
+    {
+        _InverseXAxis = false;
+        _InverseYAxis = false;
+        _EnableMusic = false;
+        _EnableSound = false; 
+
+    }
+
+    public void EnableMusic()
+    {
+        _EnableMusic = true;
+    }
+
+    public void DisableMusic()
+    {
+        _EnableMusic = false;
+    }
+
+    public void EnableSound()
+    {
+        _EnableSound = true;
+    }
+    public void DisableSound()
+    {
+        _EnableSound = false;
+    }
+
+    public void EnableInverseXAxis()
+    {
+        _InverseXAxis = true;
+    }
+
+    public void DisableInverseXAxis()
+    {
+        _InverseXAxis = false;
+    }
+
+    public void EnableInverseYAxis()
+    {
+        _InverseYAxis = true;
+    }
+    public void DisableInverseYAxis()
+    {
+        _InverseYAxis = false;
+    }
+}
---
+++ TeslaGameJam/Assets/Scripts/UI/MainMenuController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour {
+
+    public RectTransform MainMenuPanel, OptionsPanel, CreditsPanel, ExitPrompt;
+ 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void GotoPlay(){
+
+    }
+    
+public void GotoOptions(){
+        MainMenuPanel.gameObject.SetActive(false);
+        OptionsPanel.gameObject.SetActive(true);
+        CreditsPanel.gameObject.SetActive(false);
+        ExitPrompt.gameObject.SetActive(false);
+    }
+public void GotoExitPrompt(){
+    
+        MainMenuPanel.gameObject.SetActive(false);
+        OptionsPanel.gameObject.SetActive(false);
+        CreditsPanel.gameObject.SetActive(false);
+        ExitPrompt.gameObject.SetActive(true);
+    }
+
+    
+public void GotoCredits(){
+    
+        MainMenuPanel.gameObject.SetActive(false);
+        OptionsPanel.gameObject.SetActive(false);
+        CreditsPanel.gameObject.SetActive(true);
+        ExitPrompt.gameObject.SetActive(false);
+    }
+
+    
+public void GotoMainMenu(){
+    
+        MainMenuPanel.gameObject.SetActive(true);
+        OptionsPanel.gameObject.SetActive(false);
+        CreditsPanel.gameObject.SetActive(false);
+        ExitPrompt.gameObject.SetActive(false);
+    }
+
+
+
+}
